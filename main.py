d97bbcbbcea5f3551c69ec66f05a539361d24fbc
Jaasiel Abner <jabner101@gmail.com>
2023-10-05 14:23:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ screen = pygame.display.set_mode(size,pygame.RESIZABLE)
 
 control_points = []
 state = "SETTING"
+pressedCP = None
+
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
 		elif event.type == pygame.MOUSEBUTTONUP:
-			control_points.append(pygame.mouse.get_pos())
+			if state=="SETTING":
+				control_points.append(pygame.mouse.get_pos())
+			pressedCP = None
+		if pressedCP != None:
+			bez.control_points[pressedCP] = pygame.mouse.get_pos()
 
 	screen.fill(black)
 
@@ -30,18 +36,18 @@ while True:
 			pygame.draw.circle(screen, white, cp, 5, 2)
 
 	elif state == "BEZIER":
-
 		if (pygame.mouse.get_pressed() == (True, False, False)):
 			x1,y1 = pygame.mouse.get_pos()
 			for i in range(5):
 				x,y = bez.control_points[i]
 				if(abs(x-x1)<=10 and abs(y-y1)<=10):
-					bez.control_points[i] = (x1,y1)
+					pressedCP = i
+					# bez.control_points[i] = (x1,y1)
 			bez.function()
 
 		tani = bez.control_points[-1]
 		tanf = bez.control_points[-2]
-		# mult = [(tani[0]-tanf[0])/(tani[1]-tanf[1]), (tani[1]-tanf[1])/(tani[0]-tanf[0])]
+		# # mult = [(tani[0]-tanf[0])/(tani[1]-tanf[1]), (tani[1]-tanf[1])/(tani[0]-tanf[0])]
 		tanf = (0.5*tanf[0] + 0.5*tani[0], 0.5*tanf[1] +0.5*tani[1])
 
 		pygame.draw.lines(screen, purple, False, bez.points, 2)
